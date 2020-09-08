86448795daee86677afbecfa405828397e6c8603
laway1 <wayla@msu.edu>
2020-09-08 13:54:54 -0400

    Test commit!

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-background (100);
+<<<<<<< HEAD
+  createCanvas(500,500);
+  background (20,20,100);
+=======
+background (500);
+>>>>>>> 4b67157a7e603137cdadacc0488d9c79fbab0bdd
 }
 
 function draw() {
-
+line(10,10,200,200);
 }
