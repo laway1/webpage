fd792d650e661e36767e6f8d5165f26b4c659dad
laway1 <wayla@msu.edu>
2020-09-08 13:58:57 -0400

    Test

--- sketch.js
+++ sketch.js
@@ -1,18 +1,10 @@
 function setup() {
-<<<<<<< HEAD
   createCanvas(500);
   background (100,100,20);
 }
 
 function draw() {
   line(10,10,100 100)
-=======
-<<<<<<< HEAD
-  createCanvas(500,500);
-  background (20,20,100);
-=======
-background (500);
->>>>>>> 4b67157a7e603137cdadacc0488d9c79fbab0bdd
 }
 
 function draw() {
