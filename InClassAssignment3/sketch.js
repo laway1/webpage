279783792b6923af94f430a23024f72fe6d75769
laway1 <wayla@msu.edu>
2020-09-27 20:41:38 -0400

    inclassAssignment3

--- InClassAssignment3/sketch.js
+++ InClassAssignment3/sketch.js
@@ -2,45 +2,52 @@
 
 function setup() {
 createCanvas(windowWidth,windowHeight);
-background(0,0,0);
-let rcol = random(0,255);
-let gcol = random(0,255);
-let bcol = random(0,255);
-stroke(rcol,gcol,bcol);
+background(255);
 
+
+let a = random(100,windowWidth);
+let b = random(100,windowHeight);
+let c = random(0,10);
+let d = random(0,3);
+stroke(0);
+strokeWeight(20);
+//Bottom Right
+fill(0);
+rect(0,b+(height-b)/2,a,(height-b)/2);
+
+//Top left rect
+fill(250,0,0);
+rect(0,0,a,b);
+
+//Top Right
+fill(0,0,250);
+rect(a+(windowWidth-a)/2,0,(windowWidth-a)/2,b);
+
+//Bottom left
+fill(250,250,0);
+
+if(a > 500){
+  rect(0,b,a/c,height);
+}
+else{
+  rect(0,b,a*c,height);
 }
 
+//Bottom Middle
+fill(0,250,0);
+
+if(b > 500){
+rect(a,b,(windowWidth-a)/2,height);
+}
+else{
+rect(a,b,(windowWidth-a)/2,height);
+}
+
+
+
+
+}
 function draw() {
 
-  for ( let i = 0; i < width; i = i + 30) {
-    for (let j = 0; j< height; j = j + 30){
-    circle(i,j,50);
 
-      }
-
-}
-let xpos = 0;
-for (let i = height; i > 0; i = i- 10){
-  xpos= xpos + 10;
-  line(0,i,xpos,0);
-
-}
-
-let ypos = 0;
-for (let j = width; j > 0; j = j - 10){
-  ypos = ypos +10;
-  line(j,0,-200,ypos);
-}
-
-let zpos=0;
-for (let k = height; k > 0; k =k -30){
-  zpos = zpos +10;
-  line(0,k,zpos,0);
-}
-
-
-for( let i = 0; i< height; i = i+1){
-  let xlen = random (0,width);
-  line(0,i,xlen,i)
-}
 }
