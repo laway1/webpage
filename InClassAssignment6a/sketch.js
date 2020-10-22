19f541dcf2f5d7f43f206411c919e995cc34537e
laway1 <wayla@msu.edu>
2020-10-22 13:04:16 -0400

    assign6abc

--- InClassAssignment6a/sketch.js
+++ InClassAssignment6a/sketch.js
@@ -1,4 +1,5 @@
 let img;
+
 function preload(){
 
   img = loadImage('https://i.imgur.com/sS4mtgc.jpg');
@@ -13,23 +14,36 @@ function setup() {
 function draw() {
   img.loadPixels();
 
-  for(let x = 0; x< img.width; x = x+10){
-    for(let y=0; y<img.height; y = y+10){
+  for(let x = 0; x< img.width/2; x = x+1){
+    for(let y=0; y<img.height/2; y = y+1){
       let i = ( x +  y* img.width )* 4;
 
       let pixr = img.pixels[i+0];
-      let pixb = img.pixels[i+1];
-      let pixg = img.pixels[i+2];
+      let pixg = img.pixels[i+1];
+      let pixb = img.pixels[i+2];
       let pixa = img.pixels[i+3];
 
-      //fill(pixr,pixg,pixb,pixa);
-      stroke(pixr,pixg,pixb,pixa);
-
-      line(x,y,x+10,y+10);
+    if( pixr > 50){
+      img.pixels[i+0]= 0;
+    }
 
     }
   }
 
+  for(let x = img.width/2; x< img.width; x = x+1){
+    for(let y =img.height/2; y<img.height; y = y+1){
+      let i = ( x +  y* img.width )* 4;
 
+      let pixr = img.pixels[i+0];
+      let pixg = img.pixels[i+1];
+      let pixb = img.pixels[i+2];
+      let pixa = img.pixels[i+3];
+
+    img.pixels[i+2]= 0;
+
+    }
+  }
+img.updatePixels();
+image(img,0,0);
 
 }
