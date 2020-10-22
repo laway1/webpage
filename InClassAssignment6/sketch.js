87ca1534c0d213fb296f2a7cdef2af561982a250
laway1 <wayla@msu.edu>
2020-10-22 12:19:07 -0400

    Assignment 6

---
+++ InClassAssignment6/sketch.js
@@ -0,0 +1,35 @@
+let img;
+function preload(){
+
+  img = loadImage('https://i.imgur.com/sS4mtgc.jpg');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+
+}
+
+function draw() {
+  img.loadPixels();
+
+  for(let x = 0; x< img.width; x = x+10){
+    for(let y=0; y<img.height; y = y+10){
+      let i = ( x +  y* img.width )* 4;
+
+      let pixr = img.pixels[i+0];
+      let pixb = img.pixels[i+1];
+      let pixg = img.pixels[i+2];
+      let pixa = img.pixels[i+3];
+
+      //fill(pixr,pixg,pixb,pixa);
+      stroke(pixr,pixg,pixb,pixa);
+
+      line(x,y,x+10,y+10);
+
+    }
+  }
+
+
+
+}
