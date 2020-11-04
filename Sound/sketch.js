78f5d15162fa75fd4805330ce41ddfe6b9e249f3
laway1 <wayla@msu.edu>
2020-11-03 20:08:56 -0500

    guitar

--- Sound/sketch.js
+++ Sound/sketch.js
@@ -1,1 +1,13 @@
+let synth;
 
+function preload(){
+  synth = loadSound('https://laway1.github.io/webpage/Sound/synth.wav');
+
+}
+
+function setup(){
+
+createCanvas(windowWidth,windowHeight);
+background(0);
+
+}
