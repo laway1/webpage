9171c03486d7462f3bffe1ea6e841f5aed231f42
laway1 <wayla@msu.edu>
2020-11-12 17:10:11 -0500

    p31

--- Project3a/sketch.js
+++ Project3a/sketch.js
@@ -9,8 +9,8 @@ let r ;
 function setup(){
 
   createCanvas(windowWidth,windowHeight);
-background(255);
-frameRate(30);
+  background(255);
+  frameRate(30);
 
 }
 
@@ -21,12 +21,12 @@ function draw(){
  ypos =noise(yoffset) * height;
  xpos= noise(xoffset) * width;
  // no fill rectangulars with stroke weight 3.
-strokeWeight(3);
-stroke(r,0,0);
-noFill();
+ strokeWeight(3);
+  stroke(r,0,0);
+  noFill();
 // drawing retangulars at xpos & ypos
-rect(xpos,ypos,100);
+  rect(xpos,ypos,100);
 // increments of the offset values
-xoffset = xoffset + 0.01;
-yoffset = yoffset + 0.02;
+  xoffset = xoffset + 0.01;
+  yoffset = yoffset + 0.02;
 }
